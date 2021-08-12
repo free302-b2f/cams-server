576d0536143ab9c78ae0ca251d58686f343da6f5
DrBAE <amadeus.bae@gmail.com>
2021-08-12 16:49:17 +0900

    + utility.py   - refactoring: import 정렬, 미사용 변수 제거   - ModulePropertyBuilder 클래스 추가     모듈의 속성을 정의하는 도움함수

--- cams/apps/utility.py
+++ cams/apps/utility.py
@@ -2,22 +2,23 @@
 코드 중복 방지를 위한 자주 쓰는 함수 모음
 """
 
-#region ---- imports ----
-from typing import Any, Tuple, List, Dict
+# region ---- imports ----
+
+from types import FunctionType, LambdaType, ModuleType
+from typing import Any, Callable, Tuple, List, Dict
 from datetime import timedelta, datetime, timezone
 from os import path, getcwd
-import random, sys, json
+import sys
+import json
 
 from dash import callback_context as cbc
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
-import flask
-#endregion
+# endregion
 
-_pyVersion = sys.version_info[:3]
- 
+
 def caller_module(level=2):
     '''호출하는 함수가 정의된 모듈의 이름을 구한다'''
 
@@ -25,21 +26,21 @@ def caller_module(level=2):
     return f.f_globals['__name__']
 
 
-def parseDate(dateStr:str, timeStr:str=None) -> datetime:
+def parseDate(dateStr: str, timeStr: str = None) -> datetime:
     '''개별 문자열로 표현된 날짜와 시각을 `datetime`으로 파싱
 
     usage: parseDate('20210216', '12:34:56')
     '''
-    if(timeStr == None): 
+    if(timeStr == None):
         return datetime.strptime(dateStr, '%Y%m%d')
-    else: 
+    else:
         return datetime.strptime(f'{dateStr}{timeStr}', '%Y%m%d%H:%M:%S')
 #parseDate('20210216', '12:34:56')
 
 
 def getHttpInfo(flaskRequest) -> Tuple:
     '''HTTP 서버와 클라이언트 정보를 튜플로 리턴한다
-    
+
     :param flaskRequest: flask.request 객치
     :return : Tuple(host name, client ip, browser name, broser version)'''
 
@@ -50,9 +51,9 @@ def getHttpInfo(flaskRequest) -> Tuple:
     return (host, client, browser, version)
 
 
-def formatTiming(flaskRequest, dbServer:list, timing:list, dataSize) -> List[str]:
+def formatTiming(flaskRequest, dbServer: list, timing: list, dataSize) -> List[str]:
     '''주어진 정보를 서버정보와 함께 문자열 목록으로 리턴한다
-    
+
     :param flaskRequest: flask.request
     :param dbServer: list[dbms name, db server ip, db server port]
     :param timing: 소요시간 list[
@@ -71,22 +72,26 @@ def formatTiming(flaskRequest, dbServer:list, timing:list, dataSize) -> List[str
     ]
 
 
-def callback_triggered_by(ids:List[str]) -> bool:
+def callback_triggered_by(ids: List[str]) -> bool:
     '''Dash callback함수가 주어진 아이디에 의해 호출된 것인지 검사한다
 
     :param ids: 체크할 HTML 요소의 ID 목록'''
-    
-    if not cbc.triggered: return False
+
+    if not cbc.triggered:
+        return False
     else:
-        triggered_ids = [ x['prop_id'].split('.')[0] for x in cbc.triggered]
+        triggered_ids = [x['prop_id'].split('.')[0] for x in cbc.triggered]
         for id in triggered_ids:
-            if id in ids: return True
+            if id in ids:
+                return True
         return False
 
 
 _Body = List[Tuple[dbc.Label, dbc.Input]]
 _Footer = List[dbc.Button]
-def showPopup(id, header:str, body:_Body, footer:_Footer) -> dbc.Modal:
+
+
+def showPopup(id, header: str, body: _Body, footer: _Footer) -> dbc.Modal:
     '''HTML 팝업창을 만든다.
 
     :param id: 팝업창의 DOM ID
@@ -96,14 +101,14 @@ def showPopup(id, header:str, body:_Body, footer:_Footer) -> dbc.Modal:
     :return dash_bootstrap_components.Modal'''
 
     return dbc.Modal(
-    [
-        dbc.ModalHeader(header),
-        dbc.ModalBody(body),
-        dbc.ModalFooter(footer),
-    ], id=id, )
+        [
+            dbc.ModalHeader(header),
+            dbc.ModalBody(body),
+            dbc.ModalFooter(footer),
+        ], id=id, )
 
 
-def loadSettings(filePath:str) -> Dict[str, Any]:
+def loadSettings(filePath: str) -> Dict[str, Any]:
     '''설정파일을 읽어 dict를 리턴한다.
 
     :param: filePath: 설정파일의 절대경로
@@ -115,5 +120,44 @@ def loadSettings(filePath:str) -> Dict[str, Any]:
 
     return config
 
-#loadConfig('Mongo')
 
+# region ---- 모듈의 global property 정의 ----
+
+
+class ModulePropertyBuilder:
+    """모듈 속성을 정의하는 도움함수 모음"""
+
+    def __init__(self, module: ModuleType) -> None:
+        '''인스턴스 객체 생성자
+
+        :param module: 속성을 정의할 모듈  -> sys.modules[__name__]
+        '''
+
+        self._propDic = {}
+        module.__getattr__ = self.getProp
+
+        self.name = module.__name__
+        if self.name == "__init__":
+            self.name = module.__package__
+        pass
+
+    def addProp(self, propName: str, propGetter: Callable):
+        '''주어진 이름의 속성을 주어진 함수로 정의한다'''
+
+        self._propDic[propName] = propGetter
+
+    def getProp(self, propName: str, raiseIfNone:bool = True) -> Any:
+        '''주어진 이름의 속성값을 구한다
+        
+        :param raiseIfNone: 참일경우 속성값이 None이면 ValueError 예외를 던진다'''
+
+        if propName in self._propDic:
+            value = self._propDic[propName]()
+            if value is None:
+                raise ValueError(f"{self.name}.{propName} == None")
+            return value
+        raise AttributeError(
+            f"package '{self.name}' has no attribute '{propName}'"
+        )
+
+# endregion
