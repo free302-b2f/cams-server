4e886688436b64e684d3d00c42bb7c50d047b98b
DrBAE <amadeus.bae@gmail.com>
2021-12-19 12:36:46 +0900

    + apps.ircam: global 변수 제거 + apps.export: 테이블 정리

--- cams/apps/ircam.py
+++ cams/apps/ircam.py
@@ -2,7 +2,6 @@
 
 print(f"<{__name__}> loading...")
 
-# from datetime import timedelta, datetime, timezone, time, date
 from time import time as ttime, localtime, sleep
 import os
 
@@ -15,14 +14,9 @@ from db import location, sensor, user
 
 # SENSOR_ID = "B2F_CAMs_1000000000001"
 _ircam_path = "/static/ircam"
-_sensors = []
-_uris = []
-_imgs = []
-_imgInfos = []
-_outputs = []
 
 
-def getFileTime(filePath: str):
+def _getFileTime(filePath: str) -> str:
     """주어진 경로의 OS 파일시스템에 저장된 수정시각을 리턴한다"""
 
     try:
@@ -37,13 +31,12 @@ def getFileTime(filePath: str):
         return "-none-"
 
 
-def decodeJpeg():
-    """_uris 목록의 파일들에 대하여 메타데이터를 추출하여 _imgInfos에 저장한다"""
+def _decodeJpeg(sensors, imgURIs):
+    """imgURIs 목록의 파일들에 대하여 메타데이터를 추출하여 _imgInfos에 저장한다"""
 
-    global _imgInfos
-    paths = [os.path.join(app.server.root_path, *u.split("/")) for u in _uris]
-    _imgInfos = ["-none-" for s in _sensors]
-    rng = range(len(paths))
+    # sensors = fl.session.sensors
+    paths = [os.path.join(app.server.root_path, *u.split("/")) for u in imgURIs]
+    imgInfos = ["-none-" for s in sensors]
 
     for j in range(len(paths)):
         try:
@@ -54,34 +47,32 @@ def decodeJpeg():
             meta = buf[-metaLen:]
             ts = meta[0:32].decode("utf-8")
             sn = meta[32:-4].decode("utf-8")
-            info(decodeJpeg, f"ts={ts}, sn={sn}")
+            info(_decodeJpeg, f"ts={ts}, sn={sn}")
 
             ts = datetime.fromisoformat(ts).astimezone()
             ts = ts.replace(microsecond=0)
-            _imgInfos[j] = f"{sn} @ {ts.isoformat()}"
+            imgInfos[j] = f"{sn} @ {ts.isoformat()}"
         except:
-            _imgInfos[j] = f"{_sensors[j].sn} @ {getFileTime(paths[j])}"
+            imgInfos[j] = f"{sensors[j].sn} @ {_getFileTime(paths[j])}"
             pass
+    return imgInfos
 
 
-def init():
+def getImageInfos():
     """글로벌 변수 - 센서목록, IR이미지 경로 등 - 를 초기화 한다"""
 
-    global _sensors, _uris, _imgs, _outputs
+    user = fli.current_user
+    if not user:
+        return [[], [], []]
 
-    _user = fli.current_user
-    if not _user:
-        return
+    sns = user.group.sensors
+    uris = [(f"{_ircam_path}/{sns[i]}.jpg") for i in range(len(sns))]
+    infos = _decodeJpeg(sns, uris)
 
-    _farms = _user.farms
-    _sensors = []
-    for f in _farms:
-        _sensors.extend(f.sensors)
-    _uris = [(f"{_ircam_path}/{_sensors[i].sn}.jpg") for i in range(len(_sensors))]
-    _imgs = [f"apps-camera-img-{i}" for i in range(len(_sensors))]
-    decodeJpeg()
+    # _imgs = [f"apps-camera-img-{i}" for i in range(len(_sensors))]
+    # _outputs = [Output(f"apps-camera-img-{i}", "src") for i in range(len(_sensors))]
 
-    _outputs = [Output(f"apps-camera-img-{i}", "src") for i in range(len(_sensors))]
+    return [sns, uris, infos]
 
 
 # init()
@@ -90,7 +81,7 @@ def init():
 def layout():
     """dash 레이아웃"""
 
-    init()
+    sensors, uris, infos = getImageInfos()
 
     def build_img_container(index: int):
         return html.Div(
@@ -99,11 +90,11 @@ def layout():
                     # id=f"apps-camera-img-{index}",
                     id={"model": "apps-camera-img", "id": index},
                     className="apps-camera-img",
-                    **{"data-ws_uri": _uris[index]},
-                    src=_uris[index],
+                    **{"data-ws_uri": uris[index]},
+                    src=uris[index],
                 ),
                 html.Div(
-                    html.H3(f"IR Camera: {_sensors[index].name}"),
+                    html.H3(f"IR Camera: {sensors[index].name}"),
                     id="apps-camera-title-{index}",
                     className="apps-camera-title",
                 ),
@@ -111,7 +102,7 @@ def layout():
                     [
                         html.Span(""),
                         html.Span(
-                            _imgInfos[index],
+                            infos[index],
                             # id=f"apps-camera-info-{index}",
                             id={"model": "apps-camera-info", "id": index},
                             className="apps-camera-info",
@@ -127,7 +118,7 @@ def layout():
 
     return html.Div(
         [
-            *[build_img_container(i) for i in range(len(_sensors))],
+            *[build_img_container(i) for i in range(len(sensors))],
             dcc.Interval(
                 id="apps-camera-interval",
                 interval=5_000,  # in milliseconds
@@ -147,14 +138,16 @@ def layout():
 def pull():
     """pull new image"""
 
+    # return no_update
+
     # sec = datetime.now().second % 30
     sec = localtime().tm_sec % 30
     print(f"ircam checking: {sec=} @ {datetime.now().isoformat()}")
 
     if sec >= 5 and sec < 10:
         print(f"ircam reloading: {sec=} @ {datetime.now().isoformat()}")
-        decodeJpeg()
-        return [f"{u}?{ttime()}" for u in _uris], _imgInfos
+        sensors, uris, infos = getImageInfos()
+        return [f"{u}?{ttime()}" for u in uris], infos
     else:
         # return [no_update for u in _uris].extend([no_update for u in _uris])
         return no_update
